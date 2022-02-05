817929912c1c3e004fab7ec6609750e4e8d71249
the-Revolutionist <therevolutionw7@gmail.com>
2022-02-05 15:39:27 -0500

    error handling

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -4,6 +4,7 @@ import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import { reduceErrors } from './ldsUtils/ldsUtils';
 
 const COLUMNS = [
     { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
@@ -15,4 +16,8 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getAccounts)
     accounts;
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
 }
