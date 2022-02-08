9df89c082faf6c8be7853a6e61f07e623653ad3d
the-Revolutionist <therevolutionw7@gmail.com>
2022-02-08 17:47:58 -0500

    workingon handle server errors

---
+++ force-app/main/default/lwc/wireLDS/wireLDS.js
@@ -0,0 +1,34 @@
+import { LightningElement, api, wire } from "lwc";
+import { getFieldValue, getRecord } from "lightning/uiRecordApi";
+
+import INDUSTRY_FIELD from "@salesforce/schema/Account.Industry";
+import NAME_FIELD from "@salesforce/schema/Account.Name";
+import OWNER_NAME_FIELD from "@salesforce/schema/Account.Owner.Name";
+import PHONE_FIELD from "@salesforce/schema/Account.Phone";
+
+export default class WireLDS extends LightningElement {
+  @api recordId;
+
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [NAME_FIELD, INDUSTRY_FIELD],
+    optionalFields: [PHONE_FIELD, OWNER_NAME_FIELD]
+  })
+  account;
+
+  get name() {
+    return getFieldValue(this.account.data, NAME_FIELD);
+  }
+
+  get phone() {
+    return getFieldValue(this.account.data, PHONE_FIELD);
+  }
+
+  get industry() {
+    return getFieldValue(this.account.data, INDUSTRY_FIELD);
+  }
+
+  get owner() {
+    return getFieldValue(this.account.data, OWNER_NAME_FIELD);
+  }
+}
